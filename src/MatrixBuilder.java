867e27ff372ffc346612187dba5199d9ad0e16b4
DESKTOP-PQ6B4VP\123 <dminerva94@gmail.com>
2017-12-16 00:19:11 -0500

    working on matrix builder

--- src/MatrixBuilder.java
+++ src/MatrixBuilder.java
@@ -8,38 +8,38 @@ public class MatrixBuilder {
 		
 	}
 	
-	public ArrayList<String> getNodes() {
-		ArrayList<String> results = new ArrayList<String>();
-		
-				
-		
-		return results;		
-	}
-	
 	public LinkedList<LinkedList<Data>> build() {
 		LinkedList<LinkedList<Data>> matrix = new LinkedList<LinkedList<Data>>();
+		LinkedList<LinkedList<String>> d = new LinkedList<LinkedList<String>>();
 		ArrayList <String> crawled = dm.getURLs();
 		ArrayList <String> points = new ArrayList <String>();
 		
 		//give matrix its dimensions
 		for(int x = 0; x < crawled.size(); x++) {
 			matrix.add(new LinkedList<Data>());
+			d.add(new LinkedList<String>());
+		}
+		
+		for(int i = 0; i < crawled.size(); i++) {
+			
 		}
 		
 		//columns
-		for(int i = 0; i < crawled.size(); i++) {
+		/*for(int i = 0; i < crawled.size(); i++) {
 			points.clear();
 			points = dm.getPointingData(crawled.get(i));
+			System.out.println(" points: " + points.toString());
 			
 			//rows
 			for(int j = 0; j < crawled.size(); j++) {
 				for(int k = 0; k < points.size(); k++) {
+					System.out.println("crawled: " + crawled.get(j) + " - points: " + points.get(k));
 					if(crawled.get(j) == points.get(k)) {
 						matrix.get(j).add(new Data(1.0 / points.size(), j + 1, i + 1));
 					}
 				}
 			}
-		}
+		}*/
 		
 		return matrix;	
 	}
